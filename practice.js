7048a39adf8a2d511125564eb206447c510a1b89
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-23 12:51:40 +0100

    Constants Variable

--- practice.js
+++ practice.js
@@ -1,11 +1,6 @@
-// Basics
-// Initialize Camel Notations
-// Can't be a  reserved keyword
-// Should be meaningful
-// Can't start with a number
-// Can't contain a space or hyphen
-// Are case sensitive
+// Constants
+// It is not reasign;
 
-let name = 'Murat';
-let lastName = 'Akhmedov';
-console.log(name);
+const name = 'Murod';
+const lastName = 'Akhmedov';
+console.log(name, lastName);
