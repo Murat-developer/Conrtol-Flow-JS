88308e2dab6b1dc69b83a7937e67001be938bdb6
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-23 15:51:49 +0100

    Break and Continue

--- practice.js
+++ practice.js
@@ -1,7 +1,20 @@
-// For of Loop
+// Break and Continue
 
-const colors = ['red', 'blue', 'yellow'];
+let number2 = 1;
 
-for (const value of colors) {
-  console.log(value);
+while (number2 <= 6) {
+  if (number2 === 3) break;
+
+  console.log(number2);
+  number2++;
+}
+let number = 1;
+
+while (number <= 7) {
+  if (number % 2 !== 0) {
+    number++;
+    continue;
+  }
+  console.log(number);
+  number++;
 }
