5d4731e0ca6ffe9b1ce1933a26c4510a3fad77bf
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-23 14:08:55 +0100

    Arithmetic Operators

--- practice.js
+++ practice.js
@@ -1,6 +1,17 @@
-// Types of Functions
-// Calculating a value
-function square(number) {
-  return number * 3;
-}
-console.log(square(3));
+// Arithmetic Operators
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// Icrement (++)
+console.log(x++);
+console.log(x);
+
+// Decrement ( --)
+console.log(--x);
