24f125a96b9435165769b7b1ff5e25be60765912
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-23 12:42:30 +0100

    Let Variable

---
+++ practice.js
@@ -0,0 +1,11 @@
+// Basics
+// Initialize Camel Notations
+// Can't be a  reserved keyword
+// Should be meaningful
+// Can't start with a number
+// Can't contain a space or hyphen
+// Are case sensitive
+
+let name = 'Murat';
+let lastName = 'Akhmedov';
+console.log(name);
