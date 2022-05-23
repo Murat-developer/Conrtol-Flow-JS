fa3ce80d157129fed1afcdad301eb0c7abf56bf8
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-23 18:06:28 +0100

    Stars (Exercise)

--- practice.js
+++ practice.js
@@ -1,17 +1,11 @@
-// Grade
-const marks = [80, 80, 50];
+// Show Stars
 
-console.log(calculateGrade(marks));
+showStars(6);
 
-function calculateGrade(input) {
-  const average = calculateAverage(input);
-  if (average < 70) return 'A';
-  if (average < 80) return 'B';
-  return 'C';
-}
-// Single Responsibility Principle
-function calculateAverage(number) {
-  let count = 0;
-  for (let value of number) count += value;
-  return count / number.length;
+function showStars(rows) {
+  for (let row = 1; row <= rows; row++) {
+    let sum = '';
+    for (let star = 1; star <= row; star++) sum += '*';
+    console.log(row, sum);
+  }
 }
